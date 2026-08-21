1bbfc60e3e0ee858f375a4ec5902eaa38609e394
krpankaj17 <pankajsinghshekhawat752@gmail.com>
2026-08-21 18:10:02 +0530

    Removed unnecassary files

--- employee_management_Backend/scratch/list_permissions.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, 'src')
-from database import SessionLocal
-from models.user import Permission
-
-with SessionLocal() as db:
-    perms = db.query(Permission).order_by(Permission.permission_name).all()
-    for p in perms:
-        print(f"Perm: {p.permission_name} - {p.description}")
